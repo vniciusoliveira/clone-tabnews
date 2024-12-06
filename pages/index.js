87eee076cf973f8320416e284bc39cde1541eadc
Vinicius de Sousa Oliveira <115360433+vniciusoliveira@users.noreply.github.com>
2024-12-06 03:26:44 +0000

    Atulizando frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
     function Home(){
-        return <h1>Você é uma Arquiteta incrível, nunca duvide do seu potencial. Te amo ❤️ </h1>
+        return <h1>Você é uma Arquiteta incrível, nunca duvide do seu potencial, não se desespere com os desafios ou prazos, a coisas vão fluir e você tem capacidade para conseguir entregar tudo que desejar. Te amo ❤️ </h1>
     }
 
     export default Home;
