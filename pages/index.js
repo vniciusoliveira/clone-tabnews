2698e660d1919d7cf4c2298d2793b10e7c9af5e9
Vinicius de Sousa Oliveira <115360433+vniciusoliveira@users.noreply.github.com>
2024-12-06 03:09:47 +0000

    Modificando conteúdo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
     function Home(){
-        return <h1>Você é uma Arquiteta incrível, nunca duvide do seu potencial.</h1>
+        return <h1>Você é uma Arquiteta incrível, nunca duvide do seu potencial. Te amo ❤️ </h1>
     }
 
     export default Home;
